064564d46ed753d8c4c52035eee19f9c11de268b
danx <rushn24@gmail.com>
2024-09-06 22:44:36 -0400

    added Link components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'; 
 import Drink from './Drink';
 import Eat from './Eat';
 import Home from './Home';
@@ -11,13 +11,22 @@ function App() {
   
 
   return (
+    <main className="App">
         <BrowserRouter>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/eat">Eat</Link>
+                <Link to="/drink">Drink</Link>
+            </nav>
+    
             <Routes>
                 <Route exact path='/' element={<Home />} /> 
                 <Route exact path='/eat' element={<Eat />} /> 
                 <Route exact path='/drink' element={<Drink />} /> 
             </Routes>
         </BrowserRouter>
+    </main>
+        
   );
 };
 
