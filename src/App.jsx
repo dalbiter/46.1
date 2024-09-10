45f9d7baa02df36779d0ea42785d590d5ca898c4
danx <rushn24@gmail.com>
2024-09-09 21:48:51 -0400

    added Navbar component and simple styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'; 
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom'; 
 import Drink from './Drink';
 import Eat from './Eat';
 import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
   
@@ -13,12 +14,12 @@ function App() {
   return (
     <main className="App">
         <BrowserRouter>
-            <nav>
+            {/* <nav>
                 <Link to="/">Home</Link>
                 <Link to="/eat">Eat</Link>
                 <Link to="/drink">Drink</Link>
-            </nav>
-    
+            </nav> */}
+            <Navbar />
             <Routes>
                 <Route exact path='/' element={<Home />} /> 
                 <Route exact path='/eat' element={<Eat />} /> 
